de5e711f7254ff1776e755ddffaeac73b7b3a0b4
Mary-Anne Triggs <maryannetriggs@gmail.com>
2019-10-25 15:51:30 +0100

    small react edits

--- src/components/auth/LoginTraveller.js
+++ src/components/auth/LoginTraveller.js
@@ -2,7 +2,10 @@ import React from 'react'
 // import Auth from '../../lib/auth'
 
 const LoginTraveller = () => (
-  <h1>Log In Page</h1>
+  <>
+    <h1>Log In Page for the Travellers</h1>
+    <button>Log In</button>
+  </>
 )
 
 export default LoginTraveller
--- src/components/auth/RegisterTraveller.js
+++ src/components/auth/RegisterTraveller.js
@@ -2,7 +2,10 @@ import React from 'react'
 // import Auth from '../../lib/auth'
 
 const RegisterTraveller = () => (
-  <h1>Register Page</h1>
+  <>
+    <h1>Register In Page for the Travellers</h1>
+    <button>Register</button>
+  </>
 )
 
 export default RegisterTraveller

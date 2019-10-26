b4a9dc6187b7faf71e971713b63978a4edcbb435
Mary-Anne Triggs <maryannetriggs@gmail.com>
2019-10-26 19:01:24 +0100

    added unicorn seeds

---
+++ src/components/experiences/ExpCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ExpCard = ({ name, image, description, category, intensity, price, availability, time, unicorn, _id }) => (
+  <div>
+    <Link to={`/experiences/${_id}`}>
+      <div>
+        <img src={image} alt={name}/>
+        <h3>{name}</h3>
+        <h3>{description}</h3>
+        <h3>{category}</h3>
+        <h3>{intensity}</h3>
+        <h3>{price}</h3>
+        <h3>{availability}</h3>
+        <h3>{time}</h3>
+        <h3>{unicorn}</h3>
+      </div>
+    </Link>
+  </div>
+)
+
+export default ExpCard

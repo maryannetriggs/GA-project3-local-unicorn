73f456590cdef78189b4d31ccd348847301d12e6
Mary-Anne Triggs <maryannetriggs@gmail.com>
2019-10-31 12:02:35 +0000

    navbar authentication fixed

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,13 +1,46 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import Auth from '../../lib/auth'
-
+import axios from 'axios'
 
 class Navbar extends React.Component {
   constructor() {
     super()
 
+    this.state = {
+      isTraveller: false,
+      isUnicorn: false
+    }
+
     this.handleLogout = this.handleLogout.bind(this)
+  }
+
+  componentDidMount() {
+    this.checkUnicorn()
+    this.checkTraveller()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      this.checkUnicorn()
+      this.checkTraveller()
+    } 
+  }
+
+  checkUnicorn() {
+    axios.get('/api/unicorn', {
+      headers: { Authorization: `Bearer ${Auth.getToken()}` }
+    })  
+      .then(() => this.setState({ isUnicorn: true }))
+      .catch(() => this.setState({ isUnicorn: false }))
+  }
+
+  checkTraveller() {
+    axios.get('/api/traveller', {
+      headers: { Authorization: `Bearer ${Auth.getToken()}` }
+    })  
+      .then(() => this.setState({ isTraveller: true }))
+      .catch(() => this.setState({ isTraveller: false }))
   }
 
   handleLogout() {
@@ -16,24 +49,25 @@ class Navbar extends React.Component {
     )
   }
 
-  // WE NEED TO ADD IN THE IS AUTHENTICATED ON THE CORRECT LINKS BELOW LATER!
+  // Commented out navbar items below for development testing and usability purposes :)
   render() {
+    const { isUnicorn, isTraveller } = this.state
     return (
-      
+
       <nav className="navbar">
         <div className="logo-wrapper">
           <Link className="logo" to="/">🦄</Link>
         </div>
         <div className="input-group input-inline ">
-          <Link className="btn btn-link" to="/registerunicorn">Register unicorn</Link>
-          <Link className="btn btn-link" to="/registertraveller">Register traveller</Link>
+          {!Auth.isAuthenticated() && <Link className="btn btn-link" to="/registerunicorn">Register unicorn</Link>}
+          {!Auth.isAuthenticated() && <Link className="btn btn-link" to="/registertraveller">Register traveller</Link>}
 
-          <Link className="btn btn-link" to="/loginunicorn">Login unicorn</Link>
-          <Link className="btn btn-link" to="/logintraveller">Login traveller</Link>
+          {!Auth.isAuthenticated() && <Link className="btn btn-link" to="/loginunicorn">Login unicorn</Link>}
+          {!Auth.isAuthenticated() && <Link className="btn btn-link" to="/logintraveller">Login traveller</Link>}
           {/* <Link className="btn btn-link" to="/loginadmin">Login admin</Link> */}
 
-          <Link className="btn btn-link" to="/unicorn">UNICORN profile</Link>
-          <Link className="btn btn-link" to="/travellers">TRAVELLER profile</Link>
+          {isUnicorn && <Link className="btn btn-link" to="/unicorn">UNICORN profile</Link>}
+          {isTraveller && <Link className="btn btn-link" to="/travellers">TRAVELLER profile</Link>}
 
           <Link className="btn btn-link" to="/cities">CITIES</Link>
           {/* <Link className="btn btn-link" to="/unicorns">UNICORNS</Link> */}

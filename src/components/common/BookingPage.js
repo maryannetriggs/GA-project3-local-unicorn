63ac0529ba0aaef1a463968abcc84cc89059ca3b
Mary-Anne Triggs <maryannetriggs@gmail.com>
2019-10-31 17:10:10 +0000

    booking styles added

--- src/components/common/BookingPage.js
+++ src/components/common/BookingPage.js
@@ -26,42 +26,46 @@ class BookingPage extends React.Component {
     const experience = this.props.location.state.experience
     return (
       <>
-        <div>
-          <h2 className="centre">You are booking {experience.name} with {experience.unicorn.name}</h2>
+        <div className="bookingInfoWrapper centre">
+          <h2 className="centre"><span>You are booking <span className="bookingInfo">{experience.name}</span> with <span className="bookingInfo">{experience.unicorn.name}</span></span></h2>
         </div>
 
-        <section className="container">
-          <div className="columns">
-            <div className="column col-6 centre">
-              <ExpCard key={experience._id} {...experience}/>
-              <h3>Days availabile:</h3> 
-              <>
-                {experience.availability.map((avail, i) => (
-                  <h4 key={i}>{avail} </h4>
-                ))}
-              </>
-              <h3>Times availabile:</h3> 
-              <>
-                {experience.time.map((time, i) => (
-                  <h4 key={i}>{time} </h4>
-                ))}
-              </>
+        <div className="columns">
+          <div className="column col-4 centre">
+            <ExpCard key={experience._id} {...experience} 
 
-            </div>
-
-            <div className="column col-6 centre">
-              <DatePicker
-                selected={this.state.startDate}
-                onChange={this.handleChange}
-                inline
-              />
-              <Link to="/bookingrequest">
-                <button className="btn buttonColors">BOOK NOW!</button>
-              </Link>
-            </div>
-            
+            />
           </div>
-        </section>
+          <div className="column col-4 centre">
+            <h3>Days availabile:</h3> 
+            <>
+              {experience.availability.map((avail, i) => (
+                <h5 key={i}>{avail} </h5>
+              ))}
+            </>
+            <h3>Times available:</h3> 
+            <>
+              {experience.time.map((time, i) => (
+                <h5 key={i}>{time} </h5>
+              ))}
+            </>
+          </div>
+
+          <div className="column col-4 centre">
+            <DatePicker
+              selected={this.state.startDate}
+              onChange={this.handleChange}
+              inline
+            />
+          </div>
+
+        </div>
+        <div className="centre bookButton">
+          <Link to="/bookingrequest">
+            <button className="btn buttonColors">BOOK NOW!</button>
+          </Link>
+        </div>
+
       </>
     )
   }
